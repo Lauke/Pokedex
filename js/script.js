66dbc7ad87d1649ef5e82878be8667f66cb1397a
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-06 15:12:27 +0200

    added moves

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-image');
-const pokeMoves = document.querySelector('.poke-moves');
+const pokeMoveOne = document.querySelector('.poke-move-one');
+const pokeMoveTwo = document.querySelector('.poke-move-two');
+const pokeMoveThree = document.querySelector('.poke-move-three');
+const pokeMoveFour = document.querySelector('.poke-move-four');
 
 // FETCHING THE POKE API
 const api_url = ('https://pokeapi.co/api/v2/pokemon/1');
@@ -12,18 +15,31 @@ const api_url = ('https://pokeapi.co/api/v2/pokemon/1');
 // Await fetch starts an HTTP request to the API URL. Because the await keyword is present, the asynchronous function is paused until the request completes.
 // When the request completes, response is assigned with the response object of the request.
 
+// THIS DISPLAYS ONE SPECIFIC POKEMON
+
 async function getPokemon() {
     const response = await fetch(api_url);
     // waits until the request completes...
     const data = await response.json();
     console.log(data);
 
-    // FETCHING THE API DATA AND SHOW IT IN THE DIV
+    // IMPORTING THE ALL MOVES DATA
+    let allMoves = data.moves;
 
+    // IMPORTING THE API DATA AND SHOW IT IN THE DIV
     pokeName.textContent = data.name;
     pokeId.textContent = data.id;
     pokeFrontImage.src = data.sprites['front_default'];
-    pokeMoves.textContent = data.moves;
+
+    // IMPORTING THE ALL MOVES DATA (NOT THE BEST WAY, I HAVE TO IMPROVE THIS)
+
+    pokeMoveOne.textContent = data.moves[0]["move"]["name"];
+    pokeMoveTwo.textContent = data.moves[1]["move"]["name"];
+    pokeMoveThree.textContent = data.moves[2]["move"]["name"];
+    pokeMoveFour.textContent = data.moves[3]["move"]["name"];
+
 }
 
 getPokemon();
+
+// ENDING OF DISPLAY ONE SPECIFIC POKEMON
